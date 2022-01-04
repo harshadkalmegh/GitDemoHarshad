fe262811842cc03cc636cb11ac879eeb6405b4cb
hkalmegh <hkalmegh@entrata.com>
2022-01-04 20:47:11 +0530

    my git commit harshad

---
+++ DemoFirst/src/DemoClass.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\BrowserDriver\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.close();
+		
+	}
+
+}
